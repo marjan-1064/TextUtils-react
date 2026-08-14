e2b78617e5a9b268ab153d3177987337a87a8f85
marjan-1064 <marjanfatima31@gmail.com>
2026-08-14 18:00:49 +0500

    Updates

--- src/components/Alert.js
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-import useState from 'react';
-export default function Alert(props) {
-
-    return(
-    props.alert && <div class="alert alert-success" role="alert">
-    <strong>{props.alert.typ}</strong> : {props.alert.msg}
-</div>);
-}
